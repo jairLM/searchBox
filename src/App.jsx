233ef688d585d6793d482131c04370c3eba7c11e
Luis Jair Lopez Murillo <brook_98@live.com>
2023-12-21 20:57:22 -0600

    consumo de la api con useEffect y creacion del componente User

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,29 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import User from './assets/components/User';
 
 function App() {
+  const[users, setUsers] = useState([]);
 
+  useEffect( () =>{
 
+    fetch('https://randomuser.me/api/?results=10')
+    .then(response => response.json())
+    .then(data => {
+      setUsers(data.results);
+    });
 
+  },[]);
 
-  fetch('https://randomuser.me/api/?results=10')
-  .then(response => response)
-  .then(data => console.log(data))
+  
   return (
 
+    <div className="App">      
+      {users.map(user => <User name={user.name.first}/>)}
+    </div>
+   
 
-    <>
-      
-    </>
   )
 }
 
